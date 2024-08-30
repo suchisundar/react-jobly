58354b9696989d57ab6efeabe70686b35a6f8f8d
suchisundar <suchithra.sundarraman@gmail.com>
2024-08-29 17:23:36 -0700

    formatting edit

--- src/LoginForm.js
+++ src/LoginForm.js
@@ -10,8 +10,8 @@ const LoginForm = ({ setTokenAfterLogin }) => {
 	const [ response, setResponse ] = useState(false);
 	const { control, handleSubmit, reset } = useForm({
 		defaultValues: {
-			username: 'PashaLog',
-			password: 'Iloveemily'
+			username: 'testuser',
+			password: 'password'
 		}
 	});
 
